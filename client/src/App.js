d6b6f0db9a7e1d474b076cd9fd2d49c58ca08b6e
jansferr <sferrazzajane@gmail.com>
2021-03-27 08:26:07 -0400

    Search is populating

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Books from "./pages/Books";
 
 function App() {
   return (
@@ -11,11 +12,12 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
+          
+            <Route exact path="/" component={Books}>
+          
           </Route>
           <Route exact path="/books/:id">
-            <Detail />
+           
           </Route>
           <Route>
             <NoMatch />
--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,94 +1,87 @@
-import React, { useState, useEffect } from "react";
-import DeleteBtn from "../components/DeleteBtn";
+import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
+import { Input } from "../components/Form";
+import {SubmitBtn} from "../components/Search";
+import Result from "../components/Result";
 
-function Books() {
-  // Setting our component's initial state
-  const [books, setBooks] = useState([])
-  const [formObject, setFormObject] = useState({})
 
-  // Load all books and store them with setBooks
-  useEffect(() => {
-    loadBooks()
-  }, [])
 
-  // Loads all books and sets them to books
-  function loadBooks() {
-    API.getBook()
-      .then(res => 
-        setBooks(res.data)
-      )
-      .catch(err => console.log(err));
+  class Books extends Component {
+    state = {
+      books: [],
+      search:""
+
+    };
+  searchBooks = () => {
+    API.getBooks(this.state.search)
+    .then(res => {
+      console.log("this is it", res.data.items)
+      this.setState({
+        books: res.data.items,
+        search: ""
+      })
+    }).catch(err => console.log(err));
   };
-
-  // Deletes a book from the database with a given id, then reloads books from the db
-  function deleteBook(id) {
-    API.deleteBook(id)
-      .then(res => loadBooks())
-      .catch(err => console.log(err));
-  }
-
-  // Handles updating component state when the user types into the input field
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
+  
+   handleChange = event => {
+     const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    });
   };
 
   // When the form is submitted, use the API.saveBook method to save the book data
   // Then reload books from the database
-  function handleFormSubmit(event) {
+  handleFormSubmit = event => {
     event.preventDefault();
-    if (formObject.title && formObject.author) {
-      API.saveBook({
-        title: formObject.title,
-        author: formObject.author,
-        synopsis: formObject.synopsis
-      })
-        .then(res => loadBooks())
-        .catch(err => console.log(err));
-    }
+   this.searchBooks();
   };
+  saveGoogleBook = currentBook => {
+    console.log("this book", currentBook);
+    API.saveBook({
+      id: currentBook.id,
+      title: currentBook.title,
+      authors: currentBook.authors,
+      description: currentBook.description,
+      image: currentBook.image,
+      link: currentBook.link
+    })
+    .then(res => console.log("Good to go",res))
+    .catch(err =>(err));
+  }
 
-    return (
-      <>
-     
+    render() {
+      return (
+     <>
             <Jumbotron>
               <h1>Google Books Search</h1>
             </Jumbotron>
             <form>
-              <Input
-                onChange={handleInputChange}
-                name="Search"
-                placeholder="Search Author and Book(required)"
-              />
-              {/* <Input
-                onChange={handleInputChange}
-                name="author"
-                placeholder="Author (required)"
-              />
-              <TextArea
-                onChange={handleInputChange}
-                name="synopsis"
-                placeholder="Synopsis (Optional)"
-              /> */}
-              <FormBtn
-                disabled={!(formObject.author && formObject.title)}
-                onClick={handleFormSubmit}
-              >
-                Search 
-              </FormBtn>
-            </form>
-            </>
-        
-           
-      
-    );
+                    <h5>Search for books</h5>
+                    <Input 
+                        value={this.state.search}
+                        onChange={this.handleChange}
+                        name="search"
+                        placeholder="e.g. Harry Potter"
+                    />
+                    <SubmitBtn onClick={this.handleFormSubmit}/>
+                </form>
+                
+                {this.state.books.length ? (
+                    <Result 
+                    bookState={this.state.books}
+                    saveGoogleBook={this.saveGoogleBook}>
+                    </Result>
+                ) : (
+                    <div>
+                        <hr/>
+                    <p style={{fontStyle: "italic"}}>No results to display</p>
+                    </div>
+                )}
+           </>   
+      )
   }
 
-
+}
 export default Books;
